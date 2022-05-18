bd8d5431c3591fa12e7b830b3e88cbf8e0841935
MetalKaiser <metalkaiserpolanco@gmail.com>
2022-05-18 13:39:31 -0400

    Ajuste para mysqldump 8

--- app/Console/Commands/DatabaseBackup.php
+++ app/Console/Commands/DatabaseBackup.php
@@ -45,7 +45,7 @@ class DatabaseBackup extends Command
         if(!File::exists($storageAt)) {
             File::makeDirectory($storageAt, 0755, true, true);
         }
-        $command = "".env('DB_DUMP_PATH', 'mysqldump')." --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . " > " . $storageAt . $filename;
+        $command = "".env('DB_DUMP_PATH', 'mysqldump')." --column-statistics=0 --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . " > " . $storageAt . $filename;
         $returnVar = NULL;
         $output = NULL;
         exec($command, $output, $returnVar);
